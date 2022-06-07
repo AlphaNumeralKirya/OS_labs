294058664741ad72627b11c10432133c6257a2cb
AlphaNumeralKirya <78952179+AlphaNumeralKirya@users.noreply.github.com>
2022-06-07 11:53:32 +0300

    Add files via upload

---
+++ OS_Lab_5/OS_Lab_5/OS_Lab_5(Simple)/Simple.cpp
@@ -0,0 +1,77 @@
+#include <windows.h>
+#include <conio.h>
+#include <iostream>
+using namespace std;
+int main(int argc, char* argv[])
+{
+	setlocale(LC_ALL, "rus");
+	HANDLE writePipe = (HANDLE)atoi(argv[1]);
+	HANDLE readPipe = (HANDLE)atoi(argv[2]);
+	HANDLE R = OpenEvent(EVENT_ALL_ACCESS, FALSE, (LPCSTR)"R");
+	HANDLE SR = OpenEvent(EVENT_ALL_ACCESS, TRUE, (LPCSTR)"SR");
+	cout << "В ожидание получения размерности массива" << endl;
+	WaitForSingleObject(R, INFINITE);
+	int n;
+	DWORD dwBytesRead;
+	if (!ReadFile(readPipe, &n, sizeof(n), &dwBytesRead, NULL))
+	{
+		cout << "Read from the pipe failed.\n";
+		cout << "Press any key to finish.\n";
+		_getch();
+		return 0;
+	}
+	system("cls");
+	cout << "Simple получил размер массива: " << n << endl;
+	_int32* mas = new _int32[n];
+	cout << "Сгенерированный массив" << endl;
+	srand(time(0));
+	for (int i = 0; i < n; i++) {
+		mas[i] = -100 + rand() % 200;
+		cout << mas[i] << " ";
+	}
+	bool key;
+	bool keyl;
+	int rs = 0;
+	SetEvent(SR);
+	for (int i = 0; i < n; i++)
+	{
+		key = false;
+		keyl = false;
+		if (mas[i] < 0) {
+			mas[i] = mas[i] * (-1);
+			keyl = true;
+		}
+		for (int j = 2; j <= sqrt((double)mas[i]); j++)
+		{
+			if (mas[i] % j == 0 && mas[i] != j)
+			{
+				key = true;
+				break;
+			}
+		}
+		if (mas[i] == 1)
+			key = true;
+		if (key == false) {
+			if (keyl == true) {
+				mas[i] = mas[i] * (-1);
+			}
+			cout << mas[i] << endl;
+			DWORD dwBytesWritten;
+			if (!WriteFile(writePipe, &mas[i], sizeof(mas[i]), &dwBytesWritten, NULL))
+			{
+				cout << "Write to file failed.\n";
+				cout << "Press any key to finish.\n";
+				_getch();
+				return GetLastError();
+			}
+			SetEvent(SR);
+			WaitForSingleObject(R, INFINITE);
+		}
+	}
+	CloseHandle(writePipe);
+	CloseHandle(readPipe);
+	cout << "\nВсе найденные числа были отправлены\n";
+	cout << "Press any key to exit.\n";
+	_getch();
+	return 0;
+}
---
+++ OS_Lab_5/OS_Lab_5/OS_Lab_5/Server.cpp
@@ -0,0 +1,77 @@
+#include <windows.h>
+#include <conio.h>
+#include <iostream>
+using namespace std;
+int main(int argc, char* argv[])
+{
+	setlocale(LC_ALL, "rus");
+	TCHAR lpszComLine[80];
+	STARTUPINFO si;
+	PROCESS_INFORMATION pi;
+	HANDLE hWritePipe, hReadPipe;
+	HANDLE R = CreateEvent(NULL, FALSE, FALSE, (LPCSTR)"R");
+	HANDLE SR = CreateEvent(NULL, FALSE, FALSE, (LPCSTR)"SR");
+	SECURITY_ATTRIBUTES sa;
+	sa.nLength = sizeof(SECURITY_ATTRIBUTES);
+	sa.lpSecurityDescriptor = NULL;
+	sa.bInheritHandle = TRUE;
+	if (!CreatePipe(&hReadPipe, &hWritePipe, &sa, 0))
+	{
+		cout << "Create pipe failed.\n";
+		cout << "Press any key to finish.\n";
+		_getch();
+		return GetLastError();
+	}
+	ZeroMemory(&si, sizeof(STARTUPINFO));
+	si.cb = sizeof(STARTUPINFO);
+	wsprintf(lpszComLine, (LPCSTR)"C:\\Users\\kirya\\source\\repos\\OS_Lab_5\\Debug\\OS_Lab_5(Simple).exe %d %d", (int)hWritePipe, (int)hReadPipe);
+	if (!CreateProcess(NULL, lpszComLine, NULL, NULL, TRUE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi))
+	{
+		cout << "Create process failed.\n";
+		cout << "Press any key to finish.\n";
+		_getch();
+		return GetLastError();
+	}
+	CloseHandle(pi.hProcess);
+	CloseHandle(pi.hThread);
+	int n;
+	cout << "Введите размер массива" << endl;
+	cin >> n;
+	DWORD dwBytesWritten;
+	if (!WriteFile(hWritePipe, &n, sizeof(n), &dwBytesWritten, NULL))
+	{
+		cout << "Write to file failed.\n";
+		cout << "Press any key to finish.\n";
+		_getch();
+		return GetLastError();
+	}
+	cout << "Server отправил размер массива: " << n << endl;
+	SetEvent(R);
+	int k = 0;
+	WaitForSingleObject(SR, INFINITE);
+	while (WaitForSingleObject(SR, 1000) == false) {
+		k++;
+		_int32 nData;
+		DWORD dwBytesRead;
+		if (!ReadFile(hReadPipe, &nData, sizeof(nData), &dwBytesRead, NULL))
+		{
+			cout << "Read from the pipe failed.\n";
+			cout << "Press any key to finish.\n";
+			_getch();
+			return GetLastError();
+		}
+		cout << "Полученное число от Simple " << nData << endl;
+		SetEvent(R);
+	}
+	if (k == 0) {
+		cout << "Ничего не было получено" << endl;
+	}
+	else {
+		cout << "Все числа были получены" << endl;
+	}
+	CloseHandle(hWritePipe);
+	CloseHandle(hReadPipe);
+	cout << "Press any key to exit.\n";
+	_getch();
+	return 0;
+}
